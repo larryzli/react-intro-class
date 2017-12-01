fa95aed7217471d312a5348d0689f50ba7b3f7ee
Larry Li <larry.zy.li@gmail.com>
2017-12-01 14:59:55 -0600

    Finished Black Diamond

--- calculator-mini/src/App.js
+++ calculator-mini/src/App.js
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Calculator from './components/Calculator/Calculator.js';
+import React, { Component } from "react";
+import "./App.css";
+import Calculator from "./components/Calculator/Calculator.js";
 
 class App extends Component {
   render() {
-    return (
-      <Calculator />
-    );
+    return <Calculator />;
   }
 }
 
--- calculator-mini/src/components/Calculator/Calculator.js
+++ calculator-mini/src/components/Calculator/Calculator.js
@@ -1,72 +1,118 @@
-import React, { Component } from 'react';
-import calculatorImg from './calculator.png';
+import React, { Component } from "react";
+import calculatorImg from "./calculator.png";
 
 class Calculator extends Component {
-    constructor() {
-        super();
-        this.state = {
-            header: 'Calculator',
-            display: '0',
-            operator: '',
-            temp: 0,
-            resetDisplay: false
-        }
+  constructor() {
+    super();
+    this.state = {
+      header: "Calculator",
+      display: "0",
+      operator: "",
+      temp: 0,
+      resetDisplay: false
+    };
 
-        this.updateHeader = this.updateHeader.bind(this);
-        this.setDisplay = this.setDisplay.bind(this);
+    this.updateHeader = this.updateHeader.bind(this);
+    this.setDisplay = this.setDisplay.bind(this);
+    this.setOperator = this.setOperator.bind(this);
+    this.calculate = this.calculate.bind(this);
+    this.clearDisplay = this.clearDisplay.bind(this);
+  }
+  updateHeader(val) {
+    this.setState({
+      header: val
+    });
+  }
+  setDisplay(num) {
+    if (this.state.resetDisplay === true) {
+      this.clearDisplay();
     }
-    updateHeader(val) {
-        this.setState({
-            header: val
-        })
+    let display =
+      this.state.resetDisplay === true
+        ? num
+        : this.state.display === "0" ? num : this.state.display + num;
+    this.setState({
+      display: display.length <= 13 ? display : this.state.display
+    });
+  }
+  setOperator(operator) {
+    if (this.state.operator === "") {
+      this.setState({
+        temp: parseInt(this.state.display, 10),
+        operator: operator,
+        display: "0"
+      });
     }
-    setDisplay(num){
-        var display;
-        if (this.state.display === '0'){
-            this.setState({
-                display: num
-            })
-        } else {
-            this.setState({
-                display: this.state.display + num
-            })
-        }
+  }
+  calculate() {
+    if (this.state.operator === "") {
+      return;
     }
-    render() {
-        return (
-            <div id="calculator-container">
-            <input onChange={ (e) => {
-                this.updateHeader(e.target.value);
-            }} id="header-input"/>
-            <h1 id="header">{this.state.header}</h1>
-            <img className="remove-highlight" src={calculatorImg} alt="calculator" />
-            <div id="calculator-mask" className="remove-highlight">
-              <div className="output">
-                <span className="total">{this.state.display}</span>
-              </div>
-        
-              <div className="btn clear"></div>
-        
-              <div onClick={() => this.setDisplay('0')} className="btn zero"></div>
-              <div onClick={() => this.setDisplay('1')} className="btn one"></div>
-              <div onClick={() => this.setDisplay('2')} className="btn two"></div>
-              <div onClick={() => this.setDisplay('3')} className="btn three"></div>
-              <div onClick={() => this.setDisplay('4')} className="btn four"></div>
-              <div onClick={() => this.setDisplay('5')} className="btn five"></div>
-              <div onClick={() => this.setDisplay('6')} className="btn six"></div>
-              <div onClick={() => this.setDisplay('7')} className="btn seven"></div>
-              <div onClick={() => this.setDisplay('8')} className="btn eight"></div>
-              <div onClick={() => this.setDisplay('9')} className="btn nine"></div>
-        
-              <div className="btn equal"></div>
-              <div className="btn multiply"></div>
-              <div className="btn divide"></div>
-              <div className="btn subtract"></div>
-              <div className="btn add"></div>
-            </div>
+    let result =
+      this.state.operator === "+"
+        ? this.state.temp + parseInt(this.state.display, 10)
+        : this.state.operator === "-"
+          ? this.state.temp - parseInt(this.state.display, 10)
+          : this.state.operator === "/"
+            ? this.state.temp / parseInt(this.state.display, 10)
+            : this.state.operator === "*"
+              ? this.state.temp * parseInt(this.state.display, 10)
+              : null;
+    this.setState({
+      resetDisplay: true,
+      display: result
+    });
+  }
+  clearDisplay() {
+    this.setState({
+      display: "0",
+      operator: "",
+      temp: 0,
+      resetDisplay: false
+    });
+  }
+  render() {
+    return (
+      <div id="calculator-container">
+        <input
+          onChange={e => {
+            this.updateHeader(e.target.value);
+          }}
+          id="header-input"
+        />
+        <h1 id="header">{this.state.header}</h1>
+        <img
+          className="remove-highlight"
+          src={calculatorImg}
+          alt="calculator"
+        />
+        <div id="calculator-mask" className="remove-highlight">
+          <div className="output">
+            <span className="total">{this.state.display}</span>
           </div>
-        );
-    }
+
+          <div onClick={() => this.clearDisplay()} className="btn clear" />
+
+          <div onClick={() => this.setDisplay("0")} className="btn zero" />
+          <div onClick={() => this.setDisplay("1")} className="btn one" />
+          <div onClick={() => this.setDisplay("2")} className="btn two" />
+          <div onClick={() => this.setDisplay("3")} className="btn three" />
+          <div onClick={() => this.setDisplay("4")} className="btn four" />
+          <div onClick={() => this.setDisplay("5")} className="btn five" />
+          <div onClick={() => this.setDisplay("6")} className="btn six" />
+          <div onClick={() => this.setDisplay("7")} className="btn seven" />
+          <div onClick={() => this.setDisplay("8")} className="btn eight" />
+          <div onClick={() => this.setDisplay("9")} className="btn nine" />
+
+          <div onClick={() => this.calculate()} className="btn equal" />
+          <div onClick={() => this.setOperator("*")} className="btn multiply" />
+          <div onClick={() => this.setOperator("/")} className="btn divide" />
+          <div onClick={() => this.setOperator("-")} className="btn subtract" />
+          <div onClick={() => this.setOperator("+")} className="btn add" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Calculator;
